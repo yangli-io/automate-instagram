b5d4f6811b4a90b56f8b8ac7dc69a11702ca8834
yangli1990 <yangli1990@live.com.au>
2016-01-09 17:41:18 +1100

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function startTask(config){
+function startTask(config, classNames){
 	var webdriver = require('selenium-webdriver'),
     By = require('selenium-webdriver').By,
     until = require('selenium-webdriver').until;
@@ -10,13 +10,13 @@ function startTask(config){
 	    .build();
 	driver.manage().window().setSize(1920,1080)
 	driver.get('https://instagram.com/explore/tags/sydneyfoodies/');
-	driver.wait(until.elementLocated(By.className('-cx-PRIVATE-Navigation__menuLink')), 15000);
-	driver.findElement(By.css('.-cx-PRIVATE-Navigation__menuLink[href="/accounts/login/"]')).click().then(function(){
+	driver.wait(until.elementLocated(By.className('_onabe')), 15000);
+	driver.findElement(By.css('._6ssv5[href="/accounts/login/"]')).click().then(function(){
 		driver.wait(until.elementLocated(By.name('username')), 15000);
 		driver.findElement(By.name('username')).sendKeys(login.user);
 		driver.findElement(By.name('password')).sendKeys(login.pass);
-		driver.findElement(By.className('-cx-PRIVATE-SlimLoginForm__button')).click();
-		driver.wait(until.elementLocated(By.className('-cx-PRIVATE-SearchBox__inactiveSearchCaption')), 15000);
+		driver.findElement(By.tagName('button')).click();
+		driver.wait(until.elementLocated(By.css('nav[role="navigation"]')), 15000);
 
 		openTag(getTag());
 
@@ -25,8 +25,8 @@ function startTask(config){
 
 	function openTag(tag){
 		driver.get('https://instagram.com/explore/tags/' + tag + '/');
-		driver.wait(until.elementLocated(By.className('-cx-PRIVATE-TagPage__tagName')), 15000);
-		driver.findElements(By.className("-cx-PRIVATE-Photo__root")).then(function(elements_arr){ 
+		driver.wait(until.elementLocated(By.css('nav[role="navigation"]')), 15000);
+		driver.findElements(By.className(classNames.photoPreview)).then(function(elements_arr){ 
 			var i = 10;
 			function recurCall(){
 				openModalAndClick(elements_arr[i], function(){
@@ -44,12 +44,12 @@ function startTask(config){
 
 	function openModalAndClick(element, cb){
 		element.click();
-	   	driver.wait(until.elementLocated(By.className('-cx-PRIVATE-LikeButton__root')), 15000);
+	   	driver.wait(until.elementLocated(By.className(classNames.heart)), 15000);
 //   	if (Math.random() > 0.5) driver.findElement(By.className('-cx-PRIVATE-FollowButton__buttonEnabled')).click();
 		
-	   	driver.findElement(By.className('-cx-PRIVATE-LikeButton__root')).click().then(function(){
+	   	driver.findElement(By.className(classNames.heart)).click().then(function(){
 	   		setTimeout(function(){
-	   			driver.findElement(By.className('-cx-PRIVATE-Modal__closeButton')).click().then(function(){
+	   			driver.findElement(By.className(classNames.closeModal)).click().then(function(){
 			   		console.log('liked something' + login.driver);
 			   		setTimeout(cb, randomTime());
 			   	});
@@ -78,8 +78,10 @@ function startTask(config){
 
 try{
 	var configs = require('./config');
-	configs.forEach(function(config){
-		startTask(config)
+	console.log(configs);
+
+	configs.users.forEach(function(config){
+		startTask(config, configs.classNames)
 	});
 } catch (err) {
 	console.log('--error happened--');
